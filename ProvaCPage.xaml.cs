bbbd4c947c70025cbf4bab00a03408c903db429e
Lorenzo <lorenzored13@gmail.com>
2017-11-29 15:38:20 +0100

    Archiviazione iniziale del modulo ProvaC

---
+++ ProvaCPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace ProvaC
+{
+    public partial class ProvaCPage : ContentPage
+    {
+        public ProvaCPage()
+        {
+            InitializeComponent();
+
+            image.Source = "app.jpg";
+            image.Aspect = Aspect.Fill;
+
+            button = new Button
+            {
+                Text = "Click Me!",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+        }
+    }
+}
